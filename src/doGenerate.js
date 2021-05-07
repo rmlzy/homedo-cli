735459f81b4eab2414b1118a186c3a4e624fbaff
rmlzy <rmlzy@outlook.com>
2021-05-07 09:33:57 +0800

    feat: use txt template

--- src/doGenerate.js
+++ src/doGenerate.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const fs = require('fs-extra');
-const os = require('os');
 const shell = require('shelljs');
 const inquirer = require('inquirer');
 const { sleep, logger } = require('./util');
@@ -9,27 +8,17 @@ const doGenerateForm = require('./doGenerateForm');
 const doGenerateDetail = require('./doGenerateDetail');
 
 async function doGenerate() {
-  const username = os.userInfo().username;
-  logger.info(`小志：欢迎回来，${username}`);
-  await sleep(1000);
-
-  logger.info('小志：下边让我来检查一下你的环境。');
-  await sleep(1000);
-
   const viewsPath = path.resolve('src/views');
   const viewsExisted = await fs.pathExists(viewsPath);
   if (!viewsExisted) {
-    logger.info(`小志：很抱歉，我没有检测到 ${viewsPath}`);
+    logger.info(`抱歉，我没有检测到 ${viewsPath}`);
     shell.exit(1);
   }
-
-  logger.info('小志：很好，一些正常。');
-  await sleep(1000);
 
   const { type } = await inquirer.prompt({
     name: 'type',
     type: 'list',
-    message: '小志：今天想生成哪种代码？',
+    message: '你要生成哪种代码？',
     choices: [
       { name: '列表页', value: 'list' },
       { name: '表单页', value: 'form' },
--- src/doGenerateList.js
+++ src/doGenerateList.js
@@ -2,16 +2,11 @@ const path = require('path');
 const fs = require('fs-extra');
 const inquirer = require('inquirer');
 const shell = require('shelljs');
-const nunjucks = require('nunjucks');
 const ejs = require('ejs');
 const upperCase = require('uppercamelcase');
 const { sleep, logger, readTemplate, isEmptyDir } = require('./util');
 
 async function doGenerateList() {
-  await sleep(1000);
-  logger.info('小志：好的，接下来需要你回答我几个问题。');
-  await sleep(1000);
-
   const {
     name,
     needQuery,
@@ -25,7 +20,7 @@ async function doGenerateList() {
     {
       name: 'name',
       type: 'input',
-      message: '小志：起个名称吧, 比如说 my-demo。',
+      message: '请输入文件名称，比如 my-list',
       validate: (value) => {
         return !!value;
       }
@@ -33,46 +28,46 @@ async function doGenerateList() {
     {
       name: 'needQuery',
       type: 'confirm',
-      message: '小志：好的，需要查询条件吗？',
+      message: '是否需要查询条件？',
       default: true
     },
     {
       when: (ans) => ans.needQuery,
       name: 'queryCount',
       type: 'input',
-      message: '小志：好的，需要几个查询条件呢？',
+      message: '请输入查询条件个数？',
       default: 9
     },
     {
       name: 'needAction',
       type: 'confirm',
-      message: '小志：好的，需要操作按钮吗？',
+      message: '是否需要操作按钮？',
       default: true
     },
     {
       when: (ans) => ans.needAction,
       name: 'actionName',
       type: 'input',
-      message: '小志：按钮名称是什么呢（多个按钮用空格间隔）？',
+      message: '请输入按钮名称（多个按钮用空格间隔）？',
       default: ''
     },
     {
       name: 'needSelection',
       type: 'confirm',
-      message: '小志：好的，表格需要支持多选吗?',
+      message: '表格是否需要支持多选?',
       default: true
     },
     {
       name: 'needScroll',
       type: 'confirm',
-      message: '小志：好的，表格需要支持横向滚动吗?',
+      message: '表格是否需要支持横向滚动?',
       default: true
     },
     {
       when: (ans) => ans.needScroll,
       name: 'scrollX',
       type: 'input',
-      message: '小志：好的，你需要多宽的表格?',
+      message: '请输入表格宽度?',
       default: 1000,
       validate: (value) => {
         return value > 0;
@@ -86,19 +81,19 @@ async function doGenerateList() {
     shell.exit(1);
   }
 
-  logger.success('小志：开始生成代码...');
+  logger.success('开始生成代码...');
   await sleep(1000);
   try {
     const actionNames = actionName.split(' ').map((item) => item.trim());
     const upperCaseName = upperCase(name);
     const templateNames = [
-      'index.vue',
-      'list.component.html',
-      'list.component.less',
-      'list.component.ts',
-      'list.constant.ts',
-      'list.interface.ts',
-      'list.service.ts'
+      'index.vue.txt',
+      'list.component.html.txt',
+      'list.component.less.txt',
+      'list.component.ts.txt',
+      'list.constant.ts.txt',
+      'list.interface.ts.txt',
+      'list.service.ts.txt'
     ];
     for (let i = 0; i < templateNames.length; i++) {
       const tmplName = templateNames[i];
@@ -117,17 +112,17 @@ async function doGenerateList() {
       });
       await fs.outputFile(`${targetPath}/${outputName}`, compiledTmpl);
     }
-    logger.success('小志：代码生成成功！');
+    logger.success('代码生成成功！');
 
     await sleep(1000);
-    logger.success('小志：开始格式化代码...');
+    logger.success('开始格式化代码...');
     const formatCmd = `./node_modules/.bin/prettier ${targetPath} --write`;
     const formatRes = shell.exec(formatCmd, { silent: true });
     if (formatRes.code !== 0) {
-      logger.fail('小志：格式化代码失败!');
+      logger.fail('格式化代码失败!');
       shell.exit(1);
     }
-    logger.success('小志：格式化代码成功！');
+    logger.success('格式化代码成功！');
   } catch (e) {
     console.log(e);
     // pass
