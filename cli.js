c69aeb5840007ca0bfdac355b5a2a8a2912c96e2
rmlzy <rmlzy@outlook.com>
2021-05-08 16:31:41 +0800

    feat: debug

--- cli.js
+++ cli.js
@@ -1,23 +1,18 @@
 #!/usr/bin/env node
 
-const cac = require('cac');
-const update = require('update-notifier');
-const pkg = require('./package');
-const doNew = require('./src/doNew');
-const doGenerate = require('./src/doGenerate');
+const cac = require("cac");
+const update = require("update-notifier");
+const pkg = require("./package");
+const doNew = require("./src/doNew");
+const doGenerate = require("./src/doGenerate");
 
-const cli = cac('homedo');
+console.log(process.argv);
 
-cli
-  .command('new <folder>', '创建新项目')
-  .action(doNew)
-  .example('homedo new my-new-project');
+const cli = cac("homedo");
 
-cli
-  .command('generate', '生成代码')
-  .alias('g')
-  .action(doGenerate)
-  .example('homedo g');
+cli.command("new <folder>", "创建新项目").action(doNew).example("homedo new my-new-project");
+
+cli.command("generate", "生成代码").alias("g").action(doGenerate).example("homedo g");
 
 cli.version(pkg.version);
 cli.help();
